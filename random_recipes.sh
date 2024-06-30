66a3ec34d953e99083d0a92532aeafb5c8fbb53f
werterzz <40895944+werterzz@users.noreply.github.com>
2024-06-30 15:22:33 +0700

    Update random_recipes.sh

--- random_recipes.sh
+++ random_recipes.sh
@@ -1,1 +1,13 @@
-readarray -t recipes < recipes.txt; index=$(($RANDOM % ${#recipes[@]})); echo ${recipes[$index]}
+#!/bin/bash
+
+clear
+readarray -t recipes < recipes.txt
+index=$(($RANDOM % ${#recipes[@]}))
+height=$(($(stty size | cut -d " " -f 1) / 2))
+width=$(($(stty size | cut -d " " -f 2) / 2))
+y=""
+for i in $(seq 1 $height); do y+="\n"; done
+x=""
+for i in $(seq 1$width); do x+=" "; done
+echo -e "$y$x ${recipes[$index]}"
+sleep 3
