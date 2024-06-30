abf1e617c179e3e8ca141f7d1dd1a39fb71b4837
werterzz <40895944+werterzz@users.noreply.github.com>
2024-06-30 15:29:41 +0700

    Update random_recipes.sh

--- random_recipes.sh
+++ random_recipes.sh
@@ -8,6 +8,6 @@ width=$(($(stty size | cut -d " " -f 2) / 2))
 y=""
 for i in $(seq 1 $height); do y+="\n"; done
 x=""
-for i in $(seq 1$width); do x+=" "; done
+for i in $(seq 1 $width); do x+=" "; done
 echo -e "$y$x ${recipes[$index]}"
 sleep 3
